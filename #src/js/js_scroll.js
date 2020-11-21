aaa7e2dcc51041272f88de5267fef0de8f2d3105
RatmirRomanovich <ratmir1806@yandex.ru>
2020-11-21 20:34:02 +0300

    end

---
+++ #src/js/js_scroll.js
@@ -0,0 +1,41 @@
+//*скролл без использования  jquery
+// Scroll to anchors
+(function () {
+
+	const smoothScroll = function (targetEl, duration) {
+		const headerElHeight = document.querySelector('.header').clientHeight;//высота .header (нужно указать селектор хедера чтобы вычислилась его высота)
+		let target = document.querySelector(targetEl);
+		let targetPosition = target.getBoundingClientRect().top - headerElHeight; //headerElHeight - если хедер не закреплен эту переменную надо удалить.
+		let startPosition = window.pageYOffset;
+		let startTime = null;
+
+		const ease = function (t, b, c, d) {      //функция обработчик скрола
+			t /= d / 2;
+			if (t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		};
+
+		const animation = function (currentTime) {		//функция анимации
+			if (startTime === null) startTime = currentTime;
+			const timeElapsed = currentTime - startTime;
+			const run = ease(timeElapsed, startPosition, targetPosition, duration);
+			window.scrollTo(0, run);
+			if (timeElapsed < duration) requestAnimationFrame(animation);
+		};
+		requestAnimationFrame(animation);
+
+	};
+
+	const scrollTo = function () {							// подвешивание классов
+		const links = document.querySelectorAll('.js-scroll');  //js-scroll подвешиваем данный класс всем ссылкам которым необходим скролл
+		links.forEach(each => {
+			each.addEventListener('click', function () {
+				const currentTarget = this.getAttribute('href');
+				smoothScroll(currentTarget, 1000);
+			});
+		});
+	};
+	scrollTo();
+}());
+// Scroll to anchors end
--- #src/js/main.js
+++ #src/js/main.js
@@ -1,1 +1,4 @@
+//@prepros-append scrollHeader.js
 //@prepros-append burger.js
+//@prepros-append accordion.js
+//@prepros-append js_scroll.js
